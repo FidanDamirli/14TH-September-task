d2c2d0605adfc2b84857b0cae6df78c1afbf4289
Fidan <fidan@MacBook-Pro-Fidan.local>
2024-09-14 15:05:01 +0400

    first commit

---
+++ 14th September task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int Square = Squared(5);
+        Console.WriteLine(Square);
+
+        int[] numbers = { 2, 4, 5 };
+        int[] squaredNumbers = ArraySquare(numbers);
+        foreach (int number in squaredNumbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+
+    #region Task 1
+    static int Squared(int a)
+    {
+        return a * a;
+    }
+    #endregion
+
+    #region Task 2
+    static int[] ArraySquare(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = array[i] * array[i];
+        }
+        return array;
+    }
+    #endregion
+}
+
